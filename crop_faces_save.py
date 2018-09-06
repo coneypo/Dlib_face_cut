0aec162dfca42a76166d4bbfac2aafa4a7385a27
coneypo <coneypo@gmail.com>
2018-09-06 22:27:58 +0800

    crop faces V2.0

--- crop_faces_save.py
+++ crop_faces_save.py
@@ -1,28 +1,31 @@
-# 2018-01-24
-# By TimeStamp
-# #cnblogs: http://www.cnblogs.com/AdaminXie/
+# created at 2018-01-22
+# updated at 2018-09-06
+
+# Author:   coneypo
+# Blog:     http://www.cnblogs.com/AdaminXie
+# GitHub:   https://github.com/coneypo/Dlib_face_cut
 
 import dlib         # 人脸识别的库dlib
 import numpy as np  # 数据处理的库numpy
 import cv2          # 图像处理的库OpenCv
 
-# dlib预测器
+# Dlib 预测器
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 
 # 读取图像的路径
-path_read = "F:/code/python/P_Dlib_face_cut/pic/"
+path_read = "images/"
 img = cv2.imread(path_read+"test_faces_1.jpg")
 
 # 用来存储生成的单张人脸的路径
-path_save = "F:/code/python/P_Dlib_face_cut/pic/cut_single_pics/"
+path_save = "crop_faces_save_folder/"
 
-# dlib检测
-dets = detector(img, 1)
+# Dlib 检测
+faces = detector(img, 1)
 
-print("人脸数：", len(dets))
+print("人脸数：", len(faces))
 
-for k, d in enumerate(dets):
+for k, d in enumerate(faces):
 
     # 计算矩形大小
     # (x,y), (宽度width, 高度height)
@@ -40,8 +43,8 @@ for k, d in enumerate(dets):
         for j in range(width):
                 img_blank[i][j] = img[d.top()+i][d.left()+j]
 
-    #cv2.imshow("face_"+str(k+1), img_blank)
-    # 存在本地
-    cv2.imwrite(path_save+"img_face_"+str(k+1)+".jpg", img_blank)
+    # cv2.imshow("face_"+str(k+1), img_blank)
 
-#cv2.waitKey(0)
+    # 存在本地
+    print("Save to:", path_save+"img_face_"+str(k+1)+".jpg")
+    cv2.imwrite(path_save+"img_face_"+str(k+1)+".jpg", img_blank)
--- crop_faces_show.py
+++ crop_faces_show.py
@@ -1,21 +1,23 @@
-# 2018-01-22
-# By TimeStamp
-# #cnblogs: http://www.cnblogs.com/AdaminXie/
+# created at 2018-01-22
+# updated at 2018-09-06
+
+# Author:   coneypo
+# Blog:     http://www.cnblogs.com/AdaminXie
+# GitHub:   https://github.com/coneypo/Dlib_face_cut
 
 import dlib         # 人脸识别的库dlib
 import numpy as np  # 数据处理的库numpy
 import cv2          # 图像处理的库OpenCv
 
-# dlib预测器
+# Dlib 预测器
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 
 # 读取图像
-path = "F:/code/python/P_Dlib_face_cut/pic/"
-img = cv2.imread(path+"test_faces_6.jpg")
-#print("img/shape:", img.shape)
+path = "images/"
+img = cv2.imread(path+"test_faces_5.jpg")
 
-# dlib检测
+# Dlib检测
 dets = detector(img, 1)
 
 print("人脸数：", len(dets))
@@ -24,7 +26,7 @@ print("人脸数：", len(dets))
 height_max = 0
 width_sum = 0
 
-# 计算要生成的图像img_blank大小
+# 计算要生成的图像 img_blank 大小
 for k, d in enumerate(dets):
 
     # 计算矩形大小
@@ -46,8 +48,9 @@ for k, d in enumerate(dets):
         height_max = height_max
 
 # 绘制用来显示人脸的图像的大小
-print("img_blank的大小：")
-print("高度", height_max, "宽度", width_sum)
+print("img_blank的大小："
+      , '\n', "高度 / height:", height_max
+      , '\n', "宽度 / width:", width_sum)
 
 # 生成用来显示的图像
 img_blank = np.zeros((height_max, width_sum, 3), np.uint8)
@@ -68,8 +71,6 @@ for k, d in enumerate(dets):
     # 调整图像
     blank_start += width
 
-
 cv2.namedWindow("img_faces")#, 2)
 cv2.imshow("img_faces", img_blank)
-#cv2.imwrite("img_new.jpg", img_blank)
 cv2.waitKey(0)
